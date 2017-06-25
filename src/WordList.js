e72e3a77f9d2059b4363f7d0a3e12b840aeb432a
Paul Potsides <strongpauly@hotmail.com>
2017-06-25 15:51:02 +0100

    Add link to dictionary.com

--- src/WordList.js
+++ src/WordList.js
@@ -12,17 +12,23 @@ export default class WordList extends Component {
     super(props);
   }
 
+  charBoxes(word, wordIndex, wordFound) {
+    return word.split('').map( (char, charIndex) => {
+      return <div key={wordIndex + '-' + charIndex} className="charBox">{
+        wordFound ? char : ' '
+      }</div>;
+    });
+  }
+
   render() {
     return <div className="wordList">{
       this.props.wordSet.words.map( (word, wordIndex) => {
-        let wordFound = this.props.wordSet.isFound(word);
-        return <div key={wordIndex} className="wordBox">{
-            word.split('').map( (char, charIndex) => {
-              return <div key={wordIndex + '-' + charIndex} className="charBox">{
-                wordFound ? char : ' '
-              }</div>;
-            })
-        }</div>;
+        const wordFound = this.props.wordSet.isFound(word);
+        let content = this.charBoxes(word, wordIndex, wordFound);
+        if(wordFound) {
+          content = <a href={`http://www.dictionary.com/browse/${word.toLowerCase()}`} rel='noopener noreferrer' target='_blank'>{content}</a>;
+        }
+        return <div key={wordIndex} className="wordBox">{content}</div>;
       })
     }</div>;
   }
