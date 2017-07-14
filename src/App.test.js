f129463207b69f86ff9467dfb184daf97aa93a3f
Paul Potsides <strongpauly@hotmail.com>
2017-07-14 14:14:52 +0100

    Remove commented out tests.

--- src/App.test.js
+++ src/App.test.js
@@ -17,18 +17,4 @@ describe('<App>', () => {
     expect(app.find('Game[gameId=0]')).toHaveLength(0);
     expect(app.find('Game[gameId=1]')).toHaveLength(1);
   });
-
-  // it('clears completed when restarted.', () => {
-  //   const app = mount(<App width={1} height={2}/>);
-  //   //Not completed
-  //   expect(app.find('.completed')).toHaveLength(0);
-  //   //Click one cell - this should either explode or win the game as there are only two cells.
-  //   app.find('Cell td').at(0).simulate('click');
-  //   //Game should be completed
-  //   expect(app.find('.completed')).toHaveLength(1);
-  //   //Restart the game.
-  //   app.instance().restart();
-  //   //Game should be reset.
-  //   expect(app.find('.completed')).toHaveLength(0);
-  // });
 });
