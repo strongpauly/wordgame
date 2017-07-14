0cb08e8b906c4f844c759b0f01bb8335a4d7d987
Paul Potsides <strongpauly@hotmail.com>
2017-07-14 13:51:24 +0100

    Up Cell test coverage

--- src/Cell.test.js
+++ src/Cell.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cell from './Cell';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
 
 /* eslint-env jest */
 
@@ -15,4 +16,51 @@ describe('<Cell>', () => {
     expect(cell).toMatchSnapshot();
     expect(cell.find('.cell')).toHaveLength(1);
   });
+
+  it('fires select start onMouseDown', () => {
+    const onSelectStart = sinon.spy();
+    const cell = mount(<Cell x={0} y={0} char="C" onSelectStart={onSelectStart}/>);
+    cell.find('.cell').simulate('mousedown');
+    expect(onSelectStart.calledOnce).toEqual(true);
+  });
+
+  it('fires select over onMouseOver', () => {
+    const onSelectOver = sinon.spy();
+    const cell = mount(<Cell x={0} y={0} char="C" onSelectOver={onSelectOver}/>);
+    cell.find('.cell').simulate('mouseover');
+    expect(onSelectOver.calledOnce).toEqual(true);
+  });
+
+  it('fires select end onMouseUp', () => {
+    const onSelectEnd = sinon.spy();
+    const cell = mount(<Cell x={0} y={0} char="C" onSelectEnd={onSelectEnd}/>);
+    const el = cell.find('.cell');
+    el.simulate('mousedown');
+    el.simulate('mouseup');
+    expect(onSelectEnd.calledOnce).toEqual(true);
+  });
+
+  it('won\'t error if no handlers are applied', () => {
+    const cell = mount(<Cell x={0} y={0} char="C"/>);
+    const el = cell.find('.cell');
+    el.simulate('mousedown');
+    el.simulate('mouseover');
+    el.simulate('mouseup');
+  });
+
+  it('won\'t fire handlers is cell is used', () => {
+    const onSelectStart = sinon.spy();
+    const onSelectOver = sinon.spy();
+    const onSelectEnd = sinon.spy();
+    const cell = mount(<Cell x={0} y={0} char="C" used={true}
+      onSelectStart={onSelectStart} onSelectOver={onSelectOver} onSelectEnd={onSelectEnd}/>);
+    const el = cell.find('.cell');
+    el.simulate('mousedown');
+    el.simulate('mouseover');
+    el.simulate('mouseup');
+    expect(onSelectStart.called).toEqual(false);
+    expect(onSelectOver.called).toEqual(false);
+    expect(onSelectEnd.called).toEqual(false);
+  });
+
 });
