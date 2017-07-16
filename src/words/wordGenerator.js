441a901f6ebb14ef54edd94b92330752941d1495
Paul Potsides <strongpauly@hotmail.com>
2017-07-16 13:35:45 +0100

    100% Coverage

--- src/words/wordGenerator.js
+++ src/words/wordGenerator.js
@@ -1,26 +1,25 @@
-import hardRandomWord from 'random-word-of-length';
 import easyRandomWord from './easyWordGenerator';
 
 function getWordLength(min, max) {
   return Math.round(min + (Math.random() * (max - min)));
 }
 
-export default function wordGenerator(characters, minWordLength = 3, maxWordLength = 8, easy = true) {
+export default function wordGenerator(characters, minWordLength = 3, maxWordLength = 8) {
   let charactersLeft = characters;
   let wordLengths = [];
-  let randomword = easy ? easyRandomWord : hardRandomWord;
   while (charactersLeft > maxWordLength) {
     let newWordLength = getWordLength(minWordLength, maxWordLength);
     let newCharactersLeft = charactersLeft - newWordLength;
-    /* istanbul ignore if */
+    /* istanbul ignore if: randomness causes this to sometimes to not be covered */
     if (newCharactersLeft < minWordLength) {
       continue; //Loop again until we generate a length that won't leave a word too small.
     }
     wordLengths.push( newWordLength );
     charactersLeft = newCharactersLeft;
   }
+  /* istanbul ignore next: randomness causes this to sometimes to not be covered */
   if(charactersLeft > 0) {
     wordLengths.push(charactersLeft);
   }
-  return wordLengths.map( length => randomword(length));
+  return wordLengths.map( length => easyRandomWord(length));
 }
