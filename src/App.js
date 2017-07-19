569dd627eb9bbc81665b4df4df67e65ddd3474dc
Paul Potsides <strongpauly@hotmail.com>
2017-07-19 10:10:30 +0100

    Remove header.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Game from './Game';
@@ -28,10 +27,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo spinning" alt="logo" />
-          <h2>Word Game!</h2>
-        </div>
         <Game key={this.state.gameId}
             gameId={this.state.gameId}
             onRestart={this.restart}
